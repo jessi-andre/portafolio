4a9db15a43e2c771c5308ca3afab7d54a5ae7877
Jessica <jessicadaianaandre@gmail.com>
2023-11-10 22:54:22 -0300

    Nueva estructura, nuevas rutas  e instalación de Nodemon

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const index = requiere('./index.html')
-const aboutme = requiere('./aboutme.html')
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', '/index.html')))
+
+
+
+
+
+app.listen(port, () => console.log('Server running in port' + port))
