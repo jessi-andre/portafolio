3e2854b6bbeda366e15de1f993db34840eacbb44
Jessica <jessicadaianaandre@gmail.com>
2024-01-19 12:01:47 -0300

    Actualizando

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 3030;
 
+app.use(express.static("public"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "/index.html"))
+);
+app.get("/perfil", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "/perfil.html"))
+);
+app.get("/experiencia", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "/experiencias.html"))
+);
+app.get("/habilidades", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "/habilidades.html"))
+);
 
-app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', '/index.html')))
-app.get('/perfil', (req, res) => res.sendFile(path.join(__dirname, 'views', '/perfil.html')));
-app.get('/experiencia', (req, res) => res.sendFile(path.join(__dirname, 'views', '/experiencias.html')));
-app.get('/habilidades', (req, res) => res.sendFile(path.join(__dirname, 'views', '/habilidades.html')));
+app.get("/proyecto-lazlo", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "/proyecto-lazlo.html"))
+);
 
-
-
-
-
-app.listen(port, () => console.log('Server running in port' + port))
+app.listen(port, () => console.log("Server running in port" + port));
